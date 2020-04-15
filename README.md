bfce588eb4fab62902ab42c906221e9112a71c70
ReLuLu <relulu@web.de>
2020-04-15 16:16:56 +0200

    updated to clientModInitializer as we're going to work client-side only

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # LightLevelOverlay
 
-## .
-
-.
+Light Level Overlay is a modification for Minecraft clients using the FabricAPI. It is supposed to clearly reveal light levels of 7 and less without walking around watching the f3-client-light-block-value.
--- src/main/java/de/relulu/LightLevelOverlay/LiLeOvMain.java
+++ src/main/java/de/relulu/LightLevelOverlay/LiLeOvMain.java
@@ -1,11 +1,10 @@
 package de.relulu.LightLevelOverlay;
 
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.keybinding.FabricKeyBinding;
 import net.fabricmc.fabric.api.client.keybinding.KeyBindingRegistry;
 import net.fabricmc.fabric.api.event.client.ClientTickCallback;
 import net.minecraft.client.util.InputUtil;
-import net.minecraft.text.Style;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
@@ -13,7 +12,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.lwjgl.glfw.GLFW;
 
-public class LiLeOvMain implements ModInitializer {
+public class LiLeOvMain implements ClientModInitializer {
 
 	public static final Logger logger = LogManager.getLogger("lightleveloverlay");
 
@@ -22,7 +21,7 @@ public class LiLeOvMain implements ModInitializer {
 	private boolean overlayEnabled = false;
 
 	@Override
-	public void onInitialize() {
+	public void onInitializeClient() {
 
 		System.out.println("Hello Fabric world!");
 
@@ -45,10 +44,6 @@ public class LiLeOvMain implements ModInitializer {
 			if(toggleOverlay.wasPressed()) {
 
 				overlayEnabled = !overlayEnabled;
-
-				// just a debug message
-				System.out.println("was pressed!");
-
 				// just another, more meaningful debug message
 				logger.info(new TranslatableText(
 						overlayEnabled ? "debug.lloverlay.toggle_true" : "debug.lloverlay.toggle_false").asString());
@@ -58,22 +53,11 @@ public class LiLeOvMain implements ModInitializer {
 				//Toggle message
 				TranslatableText msg = new TranslatableText(
 						overlayEnabled ? "msg.lloverlay.toggle_true" : "msg.lloverlay.toggle_false");
-				msg.formatted(Formatting.YELLOW);
-				e.player.sendMessage(msg);
+				e.player.sendMessage(msg.formatted(Formatting.YELLOW));
 			}
 		});
 
 		logger.info("Light Level Overlay loaded!");
 
 	}
-
-	//public static KeyListener kl = KeyListener.getInstance();
-
-	/*
-	@Override
-	public void onInitializeClient() {
-
-	}*/
-
-
 }
